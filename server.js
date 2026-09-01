0efe41b7d9031c99c1cbb8bdacce91d569849ab0
Milan Chaudhary <Chaudharymilan419@gmail.com>
2026-09-01 18:02:29 +1000

    Restore dotenv import

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
-import "dotenv/config";
 import express from "express";
-import { connectMongoDb } from "./src/config/dbconfig.js";
+import "dotenv/config";
+import { connectMongoDb } from "./src/config/dbConfig.js";
 import transactionRouter from "./src/routers/transactionRouter.js";
 import authRouter from "./src/routers/authRouter.js";
 import cors from "cors";
-import "dotenv/config";
 
 const app = express();
 const PORT = 8000;
